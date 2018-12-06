c6effc7b2576b9e8666ddb6001aa29ee262fdd6c
Palloquin <palloquin@tripoint.nl>
2018-12-06 12:57:15 +0100

    Add project files.

---
+++ PSStubble/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Management;
+using System.Management.Automation;
+using System.Text.RegularExpressions;
+using Stubble.Core.Builders;
+using System.Web.Script.Serialization;
+
+namespace ClassLibrary1
+{
+    [Cmdlet("Invoke", "Stubble")]
+    [OutputType(typeof(string))]
+
+    public class InvokeStubbleCmdlet : Cmdlet
+    {
+        [Parameter]
+        public string template { get; set; }
+        [Parameter]
+        public string json { get; set; }
+
+
+        protected override void ProcessRecord()
+        {
+            var stubble = new StubbleBuilder().Build();
+            var serializer = new JavaScriptSerializer();
+            object data = serializer.Deserialize<IDictionary<string, object>>(json);
+            string output = stubble.Render(template, data);
+            output = CustomWhiteSpaceCleaner(output);
+            WriteObject(output);
+
+        }
+
+        internal string CustomWhiteSpaceCleaner( string input)
+        {
+
+            string output = Regex.Replace(input, "{\\|\\s*\\|}((\\r\\n)|\\r|\\n)", "");
+            output = Regex.Replace(output, "({\\|)|(\\|})", "");
+            //output = Regex.Replace(output, "\\r\\n", "");
+
+            return output;
+        }
+
+    }
+
+    
+    
+}
---
+++ PSStubble/test.ps1
@@ -0,0 +1,43 @@
+﻿Write-Host 'Importing module:' -ForegroundColor Green;
+
+Import-Module .\PSStubble.dll;
+Get-Command -module PSStubble;
+
+Write-Host 'Press any key to continue...'  -ForegroundColor Yellow;
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+
+$json = '{
+  "header": "Colors",
+  "items": [
+      {"name": "red", "first": true, "url": "#Red"},
+      {"name": "green", "link": true, "url": "#Green"},
+      {"name": "blue", "link": true, "url": "#Blue"}
+  ],
+  "empty": false
+}'
+
+
+$template = '<h1>{{header}}</h1>
+{{#bug}}
+{{/bug}}
+
+{{#items}}
+  {{#first}}
+    <li><strong>{{name}}</strong></li>
+  {{/first}}
+  {{#link}}
+    <li><a href="{{url}}">{{name}}</a></li>
+  {{/link}}
+{{/items}}
+
+{{#empty}}
+  <p>The list is empty.</p>
+{{/empty}}'
+
+clear;
+Write-Host  'Testing merge:'  -ForegroundColor Green;
+Invoke-Stubble -template $template -json $json;
+
+Write-Host 'Press any key to continue...' -ForegroundColor Yellow;
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+
